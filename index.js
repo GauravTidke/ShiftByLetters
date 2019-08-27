5af2c03ac6a162b66ae4936653ae68809ebb63eb
Gaurav <gaurav.tidke@amtrak.com>
2019-08-27 22:48:45 +0530

    code to split string and shift letters and find unchanged words after shifting

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 function ShiftByLetters(inputString, shiftBy) {
-    let i = 0;
-    if (!shiftBy) shiftBy = 1;
-    if (typeof inputString === "string") {
-        while(i < shiftBy) {
-            let x = inputString.split('');
-            x.unshift(x.pop());
-            inputString = x.join('');
-            i++;
+    let originalString = inputString.split(' ');
+    let inputStringArr = inputString.split(' ');
+    let sameWords = 0;
+    if (typeof inputString === "string" && typeof shiftBy === "number") {
+        if (!shiftBy) shiftBy = 1;
+        let i = 0;
+        for (let j = 0; j < inputStringArr.length; j++) {
+            while(i < shiftBy) {
+                let x = inputStringArr[j].split('');
+                x.unshift(x.pop());
+                inputStringArr[j] = x.join('');
+                i++;
+            }
         }
+        for (let k = 0; k < originalString.length; k++) {
+    		if ( originalString[k] == inputStringArr[k]) {
+        		sameWords++;
+        	}
+        }
+        return sameWords;
     } else {
         inputString = "invalid input";
+        return inputString;
     }
-    return inputString;
 }
 
 module.exports.ShiftByLetters = ShiftByLetters;
